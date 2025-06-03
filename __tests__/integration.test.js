73adeb2673716b92011302905f6c1e3ac97c9279
JohnsonUCSD <joc036@ucsd.edu>
2025-06-03 16:36:08 -0700

    Second test
    not finished but decks are there.

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -21,16 +21,27 @@ test("creates a Deck and saves it to the database", async () => {
 });
 
 test("creates a Deck and saves it to the database", async () => {
-    const deck = Deck("Example Title");
-    const card1 = Card("Example Topic1", "Example description1", 10);
-    const card2 = Card("Example Topic2", "Example description2", 15);
-    const card3 = Card("Example Topic3", "Example description3", 20);
-    deck.addCard(card1);
-    deck.addCard(card2);
-    deck.addCard(card3);
+    const whalesDeck = Deck("Whales");
+    const whalesCard1 = Card("What are whales?", "Whales are a marine mammal.", 10);
+    const whalesCard2 = Card("Habitat", "They live in the ocean.", 15);
+    const whalesCard3 = Card("Food they eat", "Whales like to eat krill and plankton", 20);
+    deck.addCard(whalesCard1);
+    deck.addCard(whalesCard2);
+    deck.addCard(whalesCard3);
 
-    await saveDeck(deck);
-    const savedDeck = await getDeck(deck.deckName);
-    expect(savedDeck.deckName).toStrictEqual(deck.deckName);
+    const tigersDeck = Deck("Tigers");
+    const tigerCard1 = Card("What are tigers?", "Tigers are a land mammal.", 10);
+    const tigerCard2 = Card("Habitat", "They live in the rainforests.", 15);
+    const tigerCard3 = Card("Food they eat", "Whales like to eat krill and plankton", 20);
+    deck.addCard(tigerCard1);
+    deck.addCard(tigerCard2);
+    deck.addCard(tigerCard3);
+
+    await saveDeck(whalesDeck);
+    await saveDeck(tigersDeck);
+
+
+    // const savedDeck = await getDeck(deck.deckName);
+    // expect(savedDeck.deckName).toStrictEqual(deck.deckName);
     // also expect for the actual deck but have deckName only for now until it is imported correctly
 });
